db7e154e8899dc3570f89cdc78ceb68adb0be23f
toonflash <toonflash@hotmail.com>
2018-09-26 21:43:32 +0000

    initial commit

---
+++ character-positioning.js
@@ -0,0 +1,24 @@
+function countLetters(string) {
+
+    var noSpaceString = string.split(" ").join('');
+    var letterCounts = {};
+
+    for (var i = 0; i < noSpaceString.length; i++){
+        // go one leter at a time
+        // increment count for each leter
+        // store each count into object
+
+        // if letter doesn not yet exist in object initiate to 1
+        if (!letterCounts[noSpaceString[i]]) {
+            letterCounts[noSpaceString[i]] = 1;
+        // otherwise increment
+        } else {
+            letterCounts[noSpaceString[i]] += 1;
+        }
+    }
+
+    console.log(letterCounts);
+
+ };
+ 
+ countLetters("lighthouse in the house");
